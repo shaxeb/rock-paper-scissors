085099bcc9d4e5c2b3396d5bdf8a406865c8fd85
md shahzeb <57902499+shaxeb@users.noreply.github.com>
2024-12-08 12:01:51 +0000

    Initial commmit

---
+++ script.js
@@ -0,0 +1,14 @@
+// function capitalize(str){
+
+//     str = str.toLowerCase();
+//     str = str[0].toUpperCase() + str.substring(1);
+//     console.log(str);
+// }
+
+// capitalize("ello WoRld!");
+
+function lastLetter(str){
+    console.log(str[str.length - 1]);
+}
+
+lastLetter("This is a dog.");
